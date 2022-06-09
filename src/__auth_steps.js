3572bb96bff9e2626ce34d49e1fafce6a68dd0ae
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-06-10 00:51:14 +0600

    implrement loadin and signup

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -3,7 +3,12 @@
 2. npm install firebase
 3. create firebase.init.js and import getAuth to export auth
 4. Firebase settings > Authentication > enable Email and Password Auth
-5. 
+5. Create Login, signup component, setup route
+6. Attach form field handle and form submit handler
+7. npm install --save react-firebase-hooks
+8. useCreateUserWithEmailAndPassword from react-firebase-hooks
+9. if user is create redirect to the expected page
+10. useSignInWithEmailAndPassword for sign in
 */
 
 /**
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassowrd] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event =>{
+        setPassowrd(event.target.value);
+    }
+
+    const handleConfirmPassword = event =>{
+        setConfirmPassword(event.target.value)
+    }
+
+    if(user){
+        navigate('/inventory');
+    }
+
+    const handleCreateUser = event =>{
+        event.preventDefault();
+        if(password !==confirmPassword){
+            setError('Your password did not match');
+        }if(password.length < 6){
+            setError('Password must be 6 character or grater then 6');
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
+    }
+
     return (
+        <div className='form-container'>
         <div>
-            <h2>This is signup page</h2>
+            <h2 className='form-title'>Sign Up</h2>
+            <form onSubmit={handleCreateUser}>
+                <div className='input-group'>
+                    <label htmlFor="email">Email</label>
+                    <input onBlur={handleEmailBlur} type="email" name='email' required/>
+                </div>
+                <div className='input-group'>
+                    <label htmlFor="password">Password</label>
+                    <input onBlur={handlePasswordBlur} type="password" name='password' required />
+                </div>
+                <div className='input-group'>
+                    <label htmlFor="confirmPassword">Password</label>
+                    <input onBlur={handleConfirmPassword} type="password" name='confirmPassword' required />
+                </div>
+                <p style={{color:'red'}}>{error}</p>
+                <input className='form-submit' type="submit" value="Sign Up" />
+            </form>
+            <p>Already have an Account? <Link className='form-link' to='/login'>Login</Link></p>
         </div>
+    </div>
     );
 };
 
